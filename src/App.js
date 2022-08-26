5b4ea4504c1133b27558baba39563f22d3187c90
alwe009 <63882279+alwe009@users.noreply.github.com>
2022-08-26 19:02:34 +0800

    initial commit
    re commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoList from './components/TodoList';
+import Auth from './components/authentication/Auth';
+
+function App() {
+  return (
+    <div className="App">
+      <Auth />
+    </div>
+  );
+}
+
+export default App;
